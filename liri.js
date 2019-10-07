515163401b2c144d55b250d03ef13877bfc363bb
JosephBadua <josephbadua1006@gmail.com>
2019-10-07 14:51:59 -0700

    spotify done

--- liri.js
+++ liri.js
@@ -4,8 +4,13 @@ const moment = require('moment');
 moment().format();
 var keys = require("./keys.js");
 console.log(keys);
-var spotify = (keys.spotify);
-console.log(spotify);
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: "5023d2790b7d478781ef16dde0a90a3a",
+  secret: "356f2557be5f4f5ea5bc182dc5dbeca3"
+});
+
 
 let method = process.argv[2];
 let artist = process.argv[3];
@@ -25,4 +30,27 @@ if (method == "concert-this") {
         // handle error
         console.log(error);
     })
+}
+if (method == "spotify-this-song") {
+    if (!artist) {
+        artist = "The Sign";
+      }
+    console.log(artist);
+    spotify
+    .request("https://api.spotify.com/v1/search?query=" + artist + "&type=track&market=US&offset=5&limit=10")
+    .then(function(data) {
+      var songs = data.tracks.items
+      for (var i = 0; i < songs.length; i++) {
+          console.log("----------------------------------")
+          console.log("Artist(s): " + songs[i].artists[0].name)
+          console.log("Name of the song: " + songs[i].name)
+          console.log("Preview Link: " + songs[i].preview_url)
+          console.log("This song came from the album: " + songs[i].album.name)
+          console.log("----------------------------------")
+      }
+    })
+    .catch(function(err) {
+      console.error('Error occurred: ' + err); 
+    });
+
 }
